d78c6b02dfaa700497f52544aa08949227a518ea
atiqmdaud <atiqmdaud@gmail.com>
2025-08-09 15:41:34 +0800

    Extracting presentation later services

---
+++ KedaiOnline.API/Extensions/WebApplicationBuilderExt.cs
@@ -0,0 +1,52 @@
+﻿using KedaiOnline.API.Middlewares;
+using KedaiOnline.Application.Extensions;
+using KedaiOnline.Infrastructure.Extensions;
+using Microsoft.OpenApi.Models;
+using Serilog;
+
+namespace KedaiOnline.API.Extensions;
+
+public static class WebApplicationBuilderExt
+{
+    public static void AddPresentation(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "bearerAuth"
+                }
+            },
+            //Array.Empty<string>()
+            []
+        }
+    });
+
+        });
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddControllers();
+        builder.Services.AddApplication();
+        builder.Services.AddInfrastructure(builder.Configuration);
+        builder.Services.AddScoped<ErrorHandlingMiddleware>();
+
+        builder.Host.UseSerilog((context, configuration) =>
+            configuration
+                .ReadFrom.Configuration(context.Configuration)
+        );
+
+    }
+  
+}
--- KedaiOnline.API/Program.cs
+++ KedaiOnline.API/Program.cs
@@ -1,61 +1,12 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Identity.Web;
-using KedaiOnline.Infrastructure.Extensions;
-using KedaiOnline.Infrastructure.Seeders;
-using KedaiOnline.Application.Extensions;
-using Serilog;
-using Serilog.Events;
-using Serilog.Formatting.Compact;
+using KedaiOnline.API.Extensions;
 using KedaiOnline.API.Middlewares;
 using KedaiOnline.Domain.Entities;
-using Microsoft.OpenApi.Models;
+using KedaiOnline.Infrastructure.Seeders;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer",
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "bearerAuth"
-                }
-            },
-            //Array.Empty<string>()
-            []
-        }
-    });
-
-});
-
-builder.Services.AddEndpointsApiExplorer();
-
-// Add services to the container.
-//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-//    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
-
-builder.Services.AddControllers();
-builder.Services.AddApplication();
-//builder.Configuration.GetConnectionString("KedaiOnlineDb");
-builder.Services.AddInfrastructure(builder.Configuration);
-
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
-
-builder.Host.UseSerilog((context, configuration)=>
-configuration
-    .ReadFrom.Configuration(context.Configuration)
-);
+builder.AddPresentation();
 
 var app = builder.Build();
 
@@ -71,8 +22,8 @@ app.UseSerilogRequestLogging();
 
 if (app.Environment.IsDevelopment())
 {
-app.UseSwagger();
-app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 
