fde685ff86e4caba8e0c9bb571d7c49bbf5dd613
atiqmdaud <atiqmdaud@gmail.com>
2025-08-25 20:51:13 +0800

    Controller integration tests

---
+++ test/KedaiOnline.API.Tests/Controllers/KedaiOnlineControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace KedaiOnline.API.Controllers.Tests;
+
+public class KedaiOnlineControllerTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+    public KedaiOnlineControllerTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+
+    [Fact()]
+    public async Task GetAll_ForValidRequest_Return200Ok()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/api/kedaionline?pageNumber=1&pageSize=10");
+
+        // Assert
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+    }
+
+    [Fact()]
+    public async Task GetAll_ForInvalidRequest_Return400BadRequest()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/api/kedaionline");
+
+        // Assert
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+    }
+}
